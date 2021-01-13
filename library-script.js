cdffb59222bc31e5ea58f099beb5ded1d9505002
Zain Hill <zainiscoding@gmail.com>
2021-01-13 15:10:24 +0900

    Fully implemented sign in and sign out, implemented pushing books to database, working on reading from databse

--- library-script.js
+++ library-script.js
@@ -6,19 +6,69 @@ const book = document.querySelector('.book');
 const readButton = document.querySelector('#readButton');
 
 let libraryArr = [];
-const main = document.querySelector('main');
+
+function populateLibrary() {
+  if (firebase.auth().currentUser !== null) {
+    console.log(
+      firebase
+        .firestore()
+        .collection('users')
+        .doc(getUserId())
+        .collection('library')
+    );
+  }
+}
+
+// Loads books and listens for new ones.
+function loadBooks(user) {
+  if (user) {
+    //Create the query to load the books and listen for new ones.
+    const query = firebase
+      .firestore()
+      .collection('users')
+      .doc(user.uid)
+      .collection('library');
+    //Start listening to the query.
+    query.onSnapshot(
+      (snapshot) => {
+        console.log(snapshot);
+        const books = [];
+        console.log(snapshot.docs);
+        //snapshot.docChanges() returns an empty array... Why?
+        snapshot.docChanges().forEach((change) => {
+          console.log('Working');
+          books.push(change.doc.data());
+          // console.log(book);
+          // displayMessage(
+          //   change.doc.id,
+          //   message.timestamp,
+          //   message.name,
+          //   message.text,
+          //   message.profilePicUrl,
+          //   message.imageUrl
+          // );
+          // libraryArr.push(book);
+          // console.log(libraryArr);
+        });
+        console.log(books);
+      },
+      function (error) {
+        console.log('error:' + error);
+      }
+    );
+  }
+}
+
+//Get DOM elements
+var userPicElement = document.getElementById('user-pic');
+var userNameElement = document.getElementById('user-name');
+var signInButtonElement = document.getElementById('sign-in');
+var signOutButtonElement = document.getElementById('sign-out');
 
 function signIn() {
   // Sign into Firebase using popup auth & Google as the identity provider.
   var provider = new firebase.auth.GoogleAuthProvider();
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then(function () {
-      const profilePicture = document.createElement('img');
-      profilePicture.setAttribute('src', getProfilePicUrl());
-      main.append(profilePicture);
-    });
+  firebase.auth().signInWithPopup(provider);
 }
 
 function signOut() {
@@ -29,6 +79,7 @@ function signOut() {
 function initFirebaseAuth() {
   // Listen to auth state changes.
   firebase.auth().onAuthStateChanged(authStateObserver);
+  firebase.auth().onAuthStateChanged(loadBooks);
 }
 
 // Returns the signed-in user's profile pic URL.
@@ -41,6 +92,63 @@ function getProfilePicUrl() {
 // Returns the signed-in user's display name.
 function getUserName() {
   return firebase.auth().currentUser.displayName;
+}
+
+function getUserId() {
+  return firebase.auth().currentUser.uid;
+}
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  if (user) {
+    // User is signed in!
+    // Get the signed-in user's profile pic and name.
+    var profilePicUrl = getProfilePicUrl();
+    var userName = getUserName();
+
+    // Set the user's profile pic and name.
+    userPicElement.style.backgroundImage =
+      'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+    userNameElement.textContent = userName;
+
+    // Show user's profile and sign-out button.
+    userNameElement.removeAttribute('hidden');
+    userPicElement.removeAttribute('hidden');
+    signOutButtonElement.removeAttribute('hidden');
+
+    // Hide sign-in button.
+    signInButtonElement.setAttribute('hidden', 'true');
+  } else {
+    // User is signed out!
+    // Hide user's profile and sign-out button.
+    userNameElement.setAttribute('hidden', 'true');
+    userPicElement.setAttribute('hidden', 'true');
+    signOutButtonElement.setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    signInButtonElement.removeAttribute('hidden');
+  }
+}
+
+// Adds a size to Google Profile pics URLs.
+function addSizeToGoogleProfilePic(url) {
+  if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
+    return url + '?sz=150';
+  }
+  return url;
+}
+
+function saveBookToFirebase(book) {
+  // Add a new message entry to the database.
+  return firebase
+    .firestore()
+    .collection('users')
+    .doc(getUserId())
+    .collection('library')
+    .add(book)
+    .catch(function (error) {
+      console.error('Error writing new book to database', error);
+    });
 }
 
 //  Original code
@@ -70,7 +178,7 @@ class Book {
   constructor(title, author, pages, read) {
     this.title = title;
     this.author = '\n' + author;
-    this.pages = '\n' + pages + ' pg';
+    this.pages = '\n' + pages;
     this.read = '\n' + read;
   }
 
@@ -85,17 +193,11 @@ class Book {
   }
 
   addBookToLibArr() {
-    libraryArr.push(this);
+    saveBookToFirebase({ ...this });
   }
 }
 
 const render = () => {
-  const signInWrapper = document.createElement('button');
-  signInWrapper.setAttribute('id', 'signIn');
-  signInWrapper.textContent = 'Sign In';
-  const main = document.querySelector('main');
-  main.append(signInWrapper);
-
   const library = document.createElement('div');
   library.setAttribute('id', 'library');
   libraryArr.forEach(function (book) {
@@ -133,10 +235,8 @@ const render = () => {
 
 render();
 
-signInWrapper = document.querySelector('#signIn');
-signInWrapper.addEventListener('click', (e) => {
-  signIn();
-});
+signInButtonElement.addEventListener('click', signIn);
+signOutButtonElement.addEventListener('click', signOut);
 
 newBookBtn.addEventListener('click', (e) => {
   newBookPopUp.setAttribute('class', 'newBookPopUp');
@@ -239,3 +339,8 @@ newBookBtn.addEventListener('click', (e) => {
     });
   }
 });
+
+// initialize Firebase
+initFirebaseAuth();
+
+loadBooks();
